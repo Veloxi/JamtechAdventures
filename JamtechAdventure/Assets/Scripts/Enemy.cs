2913dc0149cc05da4e52e042adc11c17ca02ed35
Ashley <ashley.crouch@rocketmail.com>
2015-09-29 14:22:09 -0400

    Created basic Enemy class

---
+++ JamtechAdventure/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int health;
+	public int damageToPlayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Damage(int damage)
+	{
+			health-=damage;
+			if (health <= 0) {
+				DestroyObject();
+			}
+	}
+}
