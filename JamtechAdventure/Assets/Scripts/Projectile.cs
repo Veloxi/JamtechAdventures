4a83d0238e276e0669d561a641a5246344c3f2ea
Veloxiac <matthewkelly95@gmail.com>
2015-09-29 13:59:42 -0400

    General Changes

---
+++ JamtechAdventure/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0f, projectileSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.tag = "Enemy") {
+            ()
+        }
+    }
+}
