f82d0960982e0885ec7da6c4237c38b34ffdcb13
Ashley <ashley.crouch@rocketmail.com>
2015-09-29 14:42:56 -0400

    Enemy Script

--- JamtechAdventure/Assets/Scripts/Enemy.cs
+++ JamtechAdventure/Assets/Scripts/Enemy.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	
+	public int health;
+	public int damageToPlayer;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
-    public void Damage() {
-        //fill this later
-    }
+	
+	void Damage(int damage)
+	{
+		health-=damage;
+		if (health <= 0) {
+			DestroyObject();
+		}
+	}
 }
