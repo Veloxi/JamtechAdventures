98c676b039a11ec1f8f2b5315485497e2d05f73a
L <thenesfero@gmail.com>
2015-10-12 21:09:11 -0400

    Collectables, Score, and ScoreUI added
    -Added Collectable script, drag and drop onto and object and change how
    much score it gives.
    -Added PointsUI script. Attached script to all playable characters.
    Users much put ScoreUI prefab into the heirarchy and link the ScoreUI
    prefab with the "text" field under PointsUI on the player for it to work
    properly.

---
+++ JamtechAdventure/Assets/Scripts/Collectable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+    public PointsUI pointsUI;
+
+
+    void Start() {
+        GameObject playerPlayer = GameObject.FindWithTag("Player");
+        pointsUI = playerPlayer.GetComponent<PointsUI>();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            pointsUI.playerPoints += 10;
+        }
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ JamtechAdventure/Assets/Scripts/PointsUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointsUI : MonoBehaviour {
+
+    public int playerPoints = 0;
+    public Text pointText;
+
+	// Use this for initialization
+	void Start () {
+        pointText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pointText.text = "Score: " + playerPoints.ToString();
+	}
+    void OnGUI() {
+
+    }
+}
