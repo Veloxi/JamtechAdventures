76ed5340bda334a87ac72b6ee937169ced16cc64
Veloxiac <matthewkelly95@gmail.com>
2015-10-18 18:08:21 -0400

    Update Level

---
+++ JamtechAdventure/Assets/Scripts/nextStage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextStage : MonoBehaviour {
+    int nextLevel;
+	// Use this for initialization
+	void Start () {
+        nextLevel = Application.loadedLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player") {
+            Application.LoadLevel(nextLevel + 1);
+        }
+    }
+}
