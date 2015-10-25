d7c745c1587d5775c4a60c44a6e6dfbc4b1d1dc5
Ashley <ashley.crouch@rocketmail.com>
2015-10-25 01:05:51 -0400

    BUTTONS ARE A GO

---
+++ JamtechAdventure/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour {
+
+    //method that the button will do when it's clicked
+	public void exitGame()
+    {
+        //closes game
+        Application.Quit();
+    }
+}
---
+++ JamtechAdventure/Assets/Scripts/StartOverButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartOverButton : MonoBehaviour {
+
+    //method that makes the button do something when it's clicked
+    public void startOver()
+    {
+        //loads the first level
+        Application.LoadLevel(0);
+    }
+}
