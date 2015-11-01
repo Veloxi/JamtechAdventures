4eedfba00b5807cccb0e3b51788d5c904a28f078
Veloxiac <matthewkelly95@gmail.com>
2015-11-01 17:28:09 -0500

    Added Pause Menu

---
+++ JamtechAdventure/Assets/Scripts/Player/EnablePauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnablePauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMenu.activeInHierarchy)
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+	}
+}
